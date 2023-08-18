b5cb1463059b1967eefb580ba6ec643eb4e924d2
Sky Yao <challenge.cool@gmail.com>
2023-08-18 14:39:15 +0800

    chore: add errorHandler

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+
 export function createApp() {
   const app = createSSRApp(App)
+	app.config.errorHandler = (err) => {
+	  console.error(err)
+	}
   return {
     app
   }
